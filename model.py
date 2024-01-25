b8c8ca36579fa584aa352c19a18676feb8f8911a
sarcasticdhruv <114219260+sarcasticdhruv@users.noreply.github.com>
2024-01-25 18:25:06 +0530

    Model updated for accuracy

--- model.py
+++ model.py
@@ -1,20 +1,25 @@
 import torch
 import torch.nn as nn
 
-
 class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
+    def __init__(self, input_size, hidden_size, num_classes, dropout_p=0.2):
         super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size) 
-        self.l2 = nn.Linear(hidden_size, hidden_size) 
+        self.l1 = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.BatchNorm1d(hidden_size),
+            nn.Dropout(dropout_p)
+        )
+        self.l2 = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.BatchNorm1d(hidden_size),
+            nn.Dropout(dropout_p)
+        )
         self.l3 = nn.Linear(hidden_size, num_classes)
-        self.relu = nn.ReLU()
-    
+
     def forward(self, x):
         out = self.l1(x)
-        out = self.relu(out)
         out = self.l2(out)
-        out = self.relu(out)
         out = self.l3(out)
-        # no activation and no softmax at the end
         return out
